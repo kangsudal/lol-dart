bd140d74a7eca849b6c4be7e62a42b367c86d533
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-09-21 01:37:05 +0900

    Bind duplicated champions by ElevatedButton

--- lib/widget/championChart.dart
+++ lib/widget/championChart.dart
@@ -34,7 +34,7 @@ class _ChampionChartState extends State {
           }
           List<Champion> champions = snapshot.data;
           return Stack(
-            children: buildMarkerList(champions),
+            children: buildPortraits(champions), //buildMarkerList(champions),
           );
         },
       ),
@@ -60,8 +60,7 @@ class _ChampionChartState extends State {
         Point p = Point(position_x, position_y);
         if (duplicatedChampionsLocationCheck[p] == null) {
           duplicatedChampionsLocationCheck[p] = [champion.name];
-        }
-        else if (duplicatedChampionsLocationCheck[p]!=null){
+        } else if (duplicatedChampionsLocationCheck[p] != null) {
           duplicatedChampionsLocationCheck[p].add(champion.name);
         }
 
@@ -81,13 +80,78 @@ class _ChampionChartState extends State {
       },
     );
 
-    print(duplicatedChampionsLocationCheck);//결과: {Point(0.5, -0.19999999999999996): [아트록스, 제이스, 렝가], Point(-0.5, -0.19999999999999996): [아리, 사일러스], Point(-0.30000000000000004, -0.3999999999999999): [아칼리, 나미],
+    print(
+        duplicatedChampionsLocationCheck); //결과: {Point(0.5, -0.19999999999999996): [아트록스, 제이스, 렝가], Point(-0.5, -0.19999999999999996): [아리, 사일러스], Point(-0.30000000000000004, -0.3999999999999999): [아칼리, 나미],
 
     //x,y가 같은 초상화는 새로운 위젯(버튼:누르면 해당 위치의 챔피언들을 보여주는)에 넣어준다
-    List<Widget> newChampionWidgetList = (championWidgetList.map((widget) {
-      return widget;
-    })).toList();
-    print("somethign");
-    return newChampionWidgetList;
+
+    return championWidgetList;
+  }
+
+  List<Align> buildPortraits(List<Champion> champions) {
+    List<Align> deduplicatedWidgetList = [];
+    Map<Point, List<Champion>> duplicatedLocationChampionsCheckMap =
+        new Map<Point, List<Champion>>();
+
+    for (Champion champion in champions) {
+      ///////////////////챔피언 좌표 계산////////////////////////////
+      double size = champion.info['attack'] + champion.info['magic'];
+      double x =
+          champion.info['attack'] - champion.info['magic']; //x축으로 ap ad 표시
+      double y = champion.info['defense']; // 방어력 y축
+      double position_x = x * 0.1; //(0~1) <- (0~10)
+      double position_y = -1 + y * 0.2;
+//              0   1    2   3     4   5   6   7   8    9    10 : 10
+//              -1 -0.8 -0.6 -0.4 -0.2  0  0.2 0.4 0.6  0.8    1
+
+      ///////////////////<챔피언 위치, 그 좌표에 존재하는 챔피언 리스트> 저장///////////
+      Point p = Point(position_x, position_y);
+      if (duplicatedLocationChampionsCheckMap[p] == null) {
+        duplicatedLocationChampionsCheckMap[p] = [champion];
+      } else if (duplicatedLocationChampionsCheckMap[p] != null) {
+        duplicatedLocationChampionsCheckMap[p].add(champion);
+      }
+    }
+//    print(duplicatedLocationChampionsCheckMap);
+    duplicatedLocationChampionsCheckMap.forEach(
+      (key, value) {
+//        print("key:$key, value:$value");
+        Point p = key;
+        List<Champion> champions = value;
+        if (champions.length == 1) {
+          deduplicatedWidgetList.add(
+            Align(
+              alignment: Alignment(p.x, p.y),
+              child: CachedNetworkImage(
+                width: 30, //size,//size * size * 0.5,
+                height: 30, //size,//size * size * 0.5,
+                imageUrl:
+                    'https://ddragon.leagueoflegends.com/cdn/$version/img/champion/${champions[0].id}.png',
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+            ),
+          );
+        } else if (champions.length > 1) {
+          deduplicatedWidgetList.add(
+            Align(
+              alignment: Alignment(p.x, p.y),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+//                  padding: EdgeInsets.all(30),
+                ),
+                child: Text(
+                  champions.length.toString(),
+                ),
+              ),
+            ),
+          );
+        }
+      },
+    );
+//    print(deduplicatedWidgetList);
+    return deduplicatedWidgetList;
   }
 }
